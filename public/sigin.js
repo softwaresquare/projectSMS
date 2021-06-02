f59c9dca3d405072a2f22741a0814d3233a9908c
Software Square <softwaresquare156@gmail.com>
2021-06-02 21:26:14 +0545

    Teacher Section Add

---
+++ public/sigin.js
@@ -0,0 +1,11 @@
+function identity()
+{
+    if(defaultCheck12.checked==true){
+       let busroute=document.getElementById("if-student").setAttribute("style","visibility:visible ; height:50px")
+    }
+    else if(defaultCheck1.checked==true){
+        let busroute=document.getElementById("if-student").setAttribute("style","visibility:hidden; height:0px")
+
+    }
+}
+    
